13ebd54651c7f92d43ffa7edbdc58ede4edf7e98
unknown <psdkei@naver.com>
2024-04-07 04:52:09 +0900

    로그인, Header 화면 구성 완료

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,15 @@ import styled from "styled-components";
 const Login = (props) => {
     return (
         <Container>
-            <Content>Content</Content>
+            <Content>
+                <CTA>
+                    <CTALogoOne src="/images/cta-logo-one.svg" alt="" />
+                    <SignUp>GET ALL THERE</SignUp>
+                    <Description>Hello 😊 Welcome to my site</Description>
+                    <CTALogoTwo src="/images/cta-logo-two.png" alt="" />
+                </CTA>
+                <BgImage />
+            </Content>
         </Container>
     )
 };
@@ -26,4 +34,60 @@ const Content = styled.div`
     padding: 80px 40px;
     height: 100%;
 `;
+const BgImage = styled.div`
+    height: 100%;
+    background-position: top;
+    background-size: cover;
+    background-repeat: no-repeat;
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+    z-index: -1;
+    background-image: url("/images/login-background.jpg")
+`;
+const CTA = styled.div`
+    max-width: 650px;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+`;
+const CTALogoOne = styled.img`
+    margin-bottom: 12px;
+    max-width: 600px;
+    min-height: 1px;
+    display: block;
+    width: 100%;
+`;
+const SignUp = styled.a`
+    font-weight: bold;
+    color: #f9f9f9;
+    background-color: #0063e5;
+    margin-bottom: 12px;
+    width: 100%;
+    letter-spacing: 1.5px;
+    font-size: 18px;
+    padding: 16.5px 0;
+    border: 1px solid transparent;
+    border-radius: 4px;
+    &:hover {
+        background-color: #0483ee;
+    }
+`
+const Description = styled.h2`
+    color: hsla(0, 0%, 95.3%, 1);
+    font-size: 11px;
+    margin: 0 0 24px;
+    line-height: 1.5;
+    letter-spacing: 1.5px;
+`;
+const CTALogoTwo = styled.img`
+    max-width: 600px;
+    margin-bottom: 20px;
+    display: inline-block;
+    vertical-align: bottom;
+    width: 100%;
+`;
+
+
 export default Login;
